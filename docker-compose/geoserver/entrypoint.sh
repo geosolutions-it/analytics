1f3cb1b9dc4317414c7753b6c9ce39c70e9f6c66
Francesco Camuffo <dev@fmac.xyz>
2024-05-24 15:33:07 +0200

    Remove geoserver resources

--- docker-compose/geoserver/entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-geoserver-plugin-download.sh ${CATALINA_BASE}/webapps/${APP_LOCATION}/WEB-INF/lib $PLUGIN_DYNAMIC_URLS
-set -m
-catalina.sh run &
-/usr/local/bin/geoserver-rest-config.sh
-fg %1
